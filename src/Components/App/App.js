8940f5f9ad984a3a905cc3b4fd8493d6c817936d
Aditya Rawal <ar.adityarawal@gmail.com>
2022-11-06 11:26:53 +0530

    This is the first popper that appears when the Share Button is clicked.

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Widget from './Widget'
+
+function App() {
+  return (
+    <div className="App">
+      Hello React App
+      <Widget/>
+    </div>
+  );
+}
+
+export default App;

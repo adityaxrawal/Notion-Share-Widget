10cc258299bd2ed57dbed6af0d6b9749ba141b73
Aditya Rawal <ar.adityarawal@gmail.com>
2022-11-07 21:23:27 +0530

    This commit is for the second Popper with the mock data

--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import './Overlay.css';
-import Popper from '@mui/material/Popper';
-// import Button from '@mui/material/Button';
-// import Box from '@mui/material/Box';
-// import Header from './Header.js';
+import React from "react";
+import Dropdown from "../mUi/Dropdown.js";
+import "../Overlay/Overlay.css";
+import HelpOutline from "@mui/icons-material/HelpOutline.js";
+import Button from "@mui/material/Button";
+import SelectPerson from "../SelectPerson/SelectPerson.js";
 
 
-function Overlay() {
-  return (
+function Overlay({handleClose}) {
+
+ return (
     <div>
-        <Popper>
-        This is another popper
-        
-        </Popper>
+      <section className="innerHeader">
+        {/* ========================================================================================================================== */}
+        {/* Header with Search Bar */}
+        <div className="innerSearchBar">
+          <SelectPerson />
+          <Dropdown />
+        </div>
+        {/* =========================================================================================================================== */}
+        {/* Body TexT Area */}
+        <textarea className="innerBody" placeholder="Your shared a page Getting Started with you"></textarea>
+        {/* =========================================================================================================================== */}
+        {/* Footer */}
+        <article className="footer2">
+          <div>
+            <div className="footerPart3">
+              <HelpOutline  /> Learn about sharing
+            </div>
+            <Button variant="contained" className="footerPart4" onClick={handleClose}>Invite</Button>
+           </div>
+        </article>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Overlay
+export default Overlay;
